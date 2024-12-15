80794484ee9062aa71cf59dd554d06162be25ed4
selk07 <selk07@ukr.net>
2024-12-15 14:22:11 +0200

    add lessons62

---
+++ server(ejs)/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const authRoute = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.use(authRoute);
+
+app.get('/', (req, res) => {
+   const theme = req.cookies.theme || 'dark';  
+   const userAuth = req.cookies.user_token ? true : false; 
+ 
+   res.render('index', { theme, userAuth });
+ });
+
+app.listen(port, () => {
+   console.log(`Server listening on port ${port}`);
+});
---
+++ server(ejs)/routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const secretKey = 'secretKey24'; 
+
+let users = [{username:"Jonh", password:"1234"}];
+console.log("Users", users)
+
+router.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  const existingUser = users.find(user => user.username === username);
+
+  if (existingUser) {
+    return res.status(400).send('This user already exists');
+  }
+  users.push({ username, password });
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  console.log("token===>", token)
+  res.cookie('user_token', token, { httpOnly: true }); 
+  res.redirect('/');  
+  console.log('register users===>', users)
+});
+
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // find in database
+  const user = users.find(user => user.username === username && user.password === password);
+  if (!user) {
+    return res.status(400).send('Error username or password');
+  }
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  res.cookie('user_token', token, { httpOnly: true }); 
+  res.redirect('/'); 
+  console.log('login user===>', users)
+});
+
+module.exports = router;
